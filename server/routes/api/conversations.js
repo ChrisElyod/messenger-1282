fffd400c45d3d317f6b1a385a071e2682d9adf25
Chris Doyle <sean.chris.doyle@gmail.com>
2021-10-26 14:29:08 -0400

    Updates for Redux functions to update state correctly, added sorting for initial render of messages

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,7 +19,6 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
       include: [
         { model: Message, order: ["createdAt", "DESC"] },
         {
@@ -68,7 +67,7 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
       conversations[i] = convoJSON;
     }
 
