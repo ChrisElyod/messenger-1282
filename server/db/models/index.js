5cf2258fd55c9acbd2187e28424c669e48a65683
ChrisElyod <sean.chris.doyle@gmail.com>
2021-11-02 09:41:52 -0400

    Issue 3 - Updated existing database models and added new model for User/Conversation cross-reference

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,8 +1,18 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const UserConversation = require("./user_conversation");
 
 // associations
+
+/**
+ * For implementation of group conversations, it goes from a one to many to a many to many relationship.
+ * 
+ * Remove lines 17-19 and uncomment the belongsToMany methods below
+ * 
+ */
+// Conversation.belongsToMany(User, { through: UserConversation })
+// User.belongsToMany(Conversation, { through: UserConversation })
 
 User.hasMany(Conversation);
 Conversation.belongsTo(User, { as: "user1" });
@@ -13,5 +23,6 @@ Conversation.hasMany(Message);
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
+  UserConversation
 };
